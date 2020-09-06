40bb1b8e670ecdbbb914876a3c1b5a6230970886
J Karthik <j.karthik@partner.samsung.com>
2020-09-06 19:25:29 +0530

    Updates

---
+++ precache-manifest.3c0b9948eabaf09c1ce22905b1f393dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8304b7a2a60323df0be20e357eaaa4dc",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "96b1fa68ee2612f1a2b7",
+    "url": "/weather-app/static/css/main.20fd924b.chunk.css"
+  },
+  {
+    "revision": "f01a834ebeb620d68321",
+    "url": "/weather-app/static/js/2.caebca62.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/weather-app/static/js/2.caebca62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96b1fa68ee2612f1a2b7",
+    "url": "/weather-app/static/js/main.d59956d8.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  },
+  {
+    "revision": "7a9adb3015b9103b37b3c7b59bcbe5c2",
+    "url": "/weather-app/static/media/background-image.7a9adb30.jpg"
+  }
+]);
